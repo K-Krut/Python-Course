df62e25d1f7142c49f6f60a3b97a1ecf2f6fb702
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-01 20:21:50 +0200

    Update Task_1.py

--- Project_5/Task_1.py
+++ Project_5/Task_1.py
@@ -1,10 +1,3 @@
-# 1. Modify the class Rational of Lab No2 to perform the following tasks:
-# - adding two Rational numbers. The result should be stored in reduced form;
-# - subtracting two Rational numbers. The result should be stored in reduced form;
-# - multiplying two Rational numbers. The result should be stored in reduced form;
-# - dividing two Rational numbers. The result should be stored in reduced form;
-# - comparison two Rational numbers.
-
 import math
 
 
