9b6e21495841fbbf0b5e99db29363ce95a34afcc
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-01 16:51:11 +0200

    Add files via upload

--- Project_5/Task_2.py
+++ Project_5/Task_2.py
@@ -13,16 +13,11 @@ import math
 
 MIN_YEAR = 1
 MAX_YEAR = 9999
-_MAXORDINAL = 3652059
 _DAYS_IN_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-_DAYS_BEFORE_MONTH = [0]  # 0 is a placeholder for indexing purposes.
+_DAYS_BEFORE_MONTH = [0]
 
 
 class Calendar:
-    """Concrete date type.
-    Operators:
-    """
     __slots__ = '_year', '_month', '_day'
 
     def __init__(self, year, month, day):
@@ -96,83 +91,188 @@ class Calendar:
     def __str__(self):
         return f'{self._year}/{self._month}/{self._day}'
 
-    def __add__(self, d=0, m=0, y=0):
-        global __day, __month, __year
-        __day, __month, __year = self._day,  self._month, self._year
-        for i in range(d):
-            __day += 1
-            if __day > _DAYS_IN_MONTH[__month]:
-                d -= i
-                if d < 0:
-                    d = 0
-                __day = 1
-                __month += 1
-                if __month > 12:
-                    __month = 1
-                    __year += 1
-                    break
-        return Calendar(__year + y, __month + m, __day + 1)
+    def __iadd__(self, other):
+        if not isinstance(other, Calendar):
+            raise ValueError('Invalid data type')
+        _day = self._day + other._day
+        _month = self._month + other._month
+        _year = self._year + other._year
 
-    def __sub__(self, d=0, m=0, y=0):
-        global __day, __month, __year
-        __day, __month, __year = self._day,  self._month, self._year
-        for i in range(d):
-            __day -= 1
-            if __day < 1:
-                d -= i
-                if d < 0:
-                    d = 0
-                __day = _DAYS_IN_MONTH[__month - 1]
-                __month -= 1
-                if __month < 0:
-                    __month = 12
-                    __year -= 1
-                    break
-        return Calendar(__year, __month, __day - 1)
+        if _day > _DAYS_IN_MONTH[self._month]:
+            _day %= _DAYS_IN_MONTH[self._month]
+            _month += 1
+            # _day -= _DAYS_IN_MONTH[self._month] > 0
+            # if self._year % 4 == 0 and (self._year % 100 != 0 or self._year % 400 == 0):
+            #     _month += 2
+        if _month > 12:
+            _month %= 12
+            _year += 1
 
-    def __iadd__(self, d=0):
-        for i in range(d):
-            self._day += 1
-            if self._day > _DAYS_IN_MONTH[self._month]:
-                d -= i
-                if d < 0:
-                    d = 0
-                self._day = 1
-                self._month += 1
-                if self._month > 12:
-                    self._month = 1
-                    self._year += 1
-                    break
-        return self
+        return Calendar(_year, _month, _day)
 
-    def __isub__(self, d=0, m=0, y=0):
-        for i in range(d):
-            self._day -= 1
-            if self._day < 1:
-                d -= i
-                if d < 0:
-                    d = 0
-                self._day = _DAYS_IN_MONTH[self._month - 1]
-                self._month -= 1
-                if self._month < 0:
-                    self._month = 12
-                    self._year -= 1
-                    break
-        return self
+    def __isub__(self, other):
+        if not isinstance(other, Calendar):
+            raise ValueError('Invalid data type')
+        _day = self._day - other._day
+        _month = self._month - other._month
+        _year = self._year - other._year
+        if _day < 0:
+            _month -= 1
+            _day = _DAYS_IN_MONTH[_month] + _day
+
+        if _month < 1:
+            _year -= 1
+            _month = 12
+        return Calendar(_year, _month, _day)
 
 
 A = Calendar(2020, 2, 12)
 B = Calendar(2020, 2, 5)
 C = Calendar(2020, 2, 5)
-D = Calendar(2021, 11, 12)
+D = Calendar(2021, 12, 2)
 E = Calendar(2022, 1, 10)
+F = Calendar(1, 1, 20)
 print(A)
 print(A == B, A <= B, A < B, A > B)
 print(A >= B, A != B, C != B)
-print(D, D + 51, E - 11)
+print(D)
+D += F
+print(D)
+D -= F
+print(D)
 
-D += 32
-print(D)
-D -= 5
-print(D)
+
+
+
+
+# print(D, D + 51, E - 11)
+# print(D, D + 51)
+
+# D += 32
+# print(D)
+# D -= 5
+# print(D)
+
+
+# def __add__(self, d=0, m=0, y=0):
+#     global __day, __month, __year
+#     __day, __month, __year = self._day,  self._month, self._year
+#     for i in range(d):
+#         __day += 1
+#         if __day > _DAYS_IN_MONTH[__month]:
+#             d -= i
+#             if d < 0:
+#                 d = 0
+#             __day = 1
+#             __month += 1
+#             if __month > 12:
+#                 __month = 1
+#                 __year += 1
+#                 break
+#     return Calendar(__year + y, __month + m, __day + 1)
+#
+# def __sub__(self, d=0, m=0, y=0):
+#     global __day, __month, __year
+#     __day, __month, __year = self._day,  self._month, self._year
+#     for i in range(d):
+#         __day -= 1
+#         if __day < 1:
+#             d -= i
+#             if d < 0:
+#                 d = 0
+#             __day = _DAYS_IN_MONTH[__month - 1]
+#             __month -= 1
+#             if __month < 0:
+#                 __month = 12
+#                 __year -= 1
+#                 break
+#     return Calendar(__year, __month, __day - 1)
+
+# def __iadd__(self, other):
+#
+#     for i in range():
+#         self._day += 1
+#         if self._day > _DAYS_IN_MONTH[self._month]:
+#             d -= i
+#             if d < 0:
+#                 d = 0
+#             self._day = 1
+#             self._month += 1
+#             if self._month > 12:
+#                 self._month = 1
+#                 self._year += 1
+#                 break
+#     return self
+#
+# def __isub__(self, d=0, m=0, y=0):
+#     for i in range(d):
+#         self._day -= 1
+#         if self._day < 1:
+#             d -= i
+#             if d < 0:
+#                 d = 0
+#             self._day = _DAYS_IN_MONTH[self._month - 1]
+#             self._month -= 1
+#             if self._month < 0:
+#                 self._month = 12
+#                 self._year -= 1
+#                 break
+#     return self
+
+# def __add__(self, other):
+#     if not isinstance(other, Calendar):
+#         raise ValueError('Invalid data type')
+#     _day = self._day
+#     _month = self._month
+#     _year = self._year
+#     for i in range(other._day):
+#         _day += 1
+#         if _day > _DAYS_IN_MONTH[_month]:
+#             other._day -= i
+#             if other._day < 0:
+#                 other._day = 0
+#             _day = 1
+#             _month += 1
+#             if _month > 12:
+#                  _month = 1
+#                  _year += 1
+#                  break
+#     if _month > 12:
+#         while 1 <= _month <= 12:
+#             _month -= 12
+#             _year += 1
+
+# def __add__(self, d=0, m=0, y=0):
+#     global __day, __month, __year
+#     __day, __month, __year = self._day,  self._month, self._year
+#     for i in range(d):
+#         __day += 1
+#         if __day > _DAYS_IN_MONTH[__month]:
+#             d -= i
+#             if d < 0:
+#                 d = 0
+#             __day = 1
+#             __month += 1
+#             if __month > 12:
+#                 __month = 1
+#                 __year += 1
+#                 break
+#     return Calendar(__year + y, __month + m, __day + 1)
+#
+# def __sub__(self, d=0, m=0, y=0):
+#     global __day, __month, __year
+#     __day, __month, __year = self._day,  self._month, self._year
+#     for i in range(d):
+#         __day -= 1
+#         if __day < 1:
+#             d -= i
+#             if d < 0:
+#                 d = 0
+#             __day = _DAYS_IN_MONTH[__month - 1]
+#             __month -= 1
+#             if __month < 0:
+#                 __month = 12
+#                 __year -= 1
+#                 break
+#     return Calendar(__year, __month, __day - 1)
 
