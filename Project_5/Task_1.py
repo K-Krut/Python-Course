cfa250600e4113b98392397c76c354be149610c4
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-01 22:14:33 +0200

    Add files via upload

--- Project_5/Task_1.py
+++ Project_5/Task_1.py
@@ -1,27 +1,72 @@
+# 1. Modify the class Rational of Lab No2 to perform the following tasks:
+# - adding two Rational numbers. The result should be stored in reduced form;
+# - subtracting two Rational numbers. The result should be stored in reduced form;
+# - multiplying two Rational numbers. The result should be stored in reduced form;
+# - dividing two Rational numbers. The result should be stored in reduced form;
+# - comparison two Rational numbers.
+
 import math
 
 
 class Rational:
-   
+    """Class for creating right fractions
+    Operators:
+    __str__
+    __eq__, __le__, __lt__, __ge__, __gt__, __ne__, __bool__
+    __add__, __sub__, __mul__, __truediv__
+    Methods:
+    _cmp(), float_point_format()
+    """
     def __init__(self, numerator, denominator=1):
+        """Constructor
+        :param numerator
+        :param denominator, by default 1
+        :raise  ValueError, ZeroDivisionError
+        """
         if not (isinstance(numerator, int) or isinstance(denominator, int)):
             raise TypeError("Type Error")
         if denominator == 0:
             raise ZeroDivisionError(f'Rational({numerator}, 0)')
-        g = math.gcd(numerator, denominator)
-        self._numerator = numerator // g
-        self._denominator = denominator // g
+        self._numerator = numerator
+        self._denominator = denominator
+        # g = math.gcd(numerator, denominator)
+        # self._numerator = numerator // g
+        # self._denominator = denominator // g
+
+    @property
+    def numerator(self):
+        return self._numerator
+
+    @numerator.setter
+    def numerator(self, num):
+        self._numerator = num
+
+    @property
+    def denominator(self):
+        return self._denominator
+
+    @denominator.setter
+    def denominator(self, num):
+        self._denominator = num
+
+    def _reducing(self):
+        g = math.gcd(self._numerator, self._denominator)
+        return self._numerator // g, self._denominator // g
 
     def __add__(a, b):
+        """ a + b """
         return Rational(a._numerator * b._denominator + a._denominator * b._numerator, a._denominator * b._denominator)
 
     def __sub__(a, b):
+        """ a - b """
         return Rational(a._numerator * b._denominator - a._denominator * b._numerator, a._denominator * b._denominator)
 
     def __mul__(a, b):
+        """ a * b """
         return Rational(a._numerator * b._numerator, a._denominator * b._denominator)
 
     def __truediv__(a, b):
+        """ a / b"""
         return Rational(a._numerator * b._denominator, b._numerator * a._denominator)
 
     # __iadd__ = __add__
@@ -29,49 +74,73 @@ class Rational:
     # __imul__ = __mul__
     # __idiv__ = __truediv__
 
-    def _cmp(self, other):
-        a = self._numerator / self._denominator
-        b = other._numerator / other._denominator
-        return 0 if a == b else 1 if a > b else -1
+    def _cmp(a, b):
+        """Function for comparison"""
+        if a._denominator == b._denominator:
+            return 0 if a._numerator == b._numerator else 1 if a._numerator > b._numerator else -1
+        if a._numerator == b._numerator:
+            return 0 if a._numerator == b._numerator else 1 if a._numerator < b._numerator else -1
+        g = math.lcm(a._denominator, b._denominator)
+        A_num = a.numerator * (g / a.denominator)
+        B_num = b.numerator * (g / b.denominator)
+        return 0 if A_num == B_num else 1 if A_num > B_num else -1
 
     def __eq__(a, b):
+        """a == b"""
         if not isinstance(b, Rational):
             raise ValueError('Invalid data type')
         return a._cmp(b) == 0
 
     def __le__(a, b):
+        """a <= b"""
         if not isinstance(b, Rational):
             raise ValueError('Invalid data type')
         return a._cmp(b) <= 0
 
     def __lt__(a, b):
+        """a < b"""
         if not isinstance(b, Rational):
             raise ValueError('Invalid data type')
         return a._cmp(b) < 0
 
     def __ge__(a, b):
+        """a >= b"""
         if not isinstance(b, Rational):
             raise ValueError('Invalid data type')
         return a._cmp(b) >= 0
 
     def __gt__(a, b):
+        """a > b"""
         if not isinstance(b, Rational):
             raise ValueError('Invalid data type')
         return a._cmp(b) > 0
 
     def __ne__(a, b):
+        """ a != b """
         if not isinstance(b, Rational):
             raise ValueError('Invalid data type')
         return a._cmp(b) != 0
 
-    def __bool__(a):
-        return bool(a._numerator)
-
     def __str__(self):
+        """Return the string representation of the fraction
+               :return: String representation.
+               :rtype: str
+               **Example**:
+               .. doctest::
+                   # >>>print(Fraction(4, 12))
+                   1/3
+        """
         return f'{self._numerator}/{self._denominator}'
-   
-    @property
-    def get_float_point_format(self):
+
+    def float_point_format(self):
+        """Return the string representation of the fraction
+               :return: String representation.
+               :rtype: str
+               **Example**:
+               .. doctest::
+                   # >>>print(Rational(8, 10).float_point_format)
+                   0.8
+        """
         return f'{round(self._numerator / self._denominator, 2)}'
 
 
@@ -96,7 +165,7 @@ print(
 
 print(
     fraction_1,
-    fraction_1.get_float_point_format
+    fraction_1.float_point_format()
 )
 
 print(
