379f6d55f4f8ea1a582945c34b67c73554b2d50b
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-08 13:48:37 +0200

    Update Task_1.py

--- Classwork_6/Task_1.py
+++ Classwork_6/Task_1.py
@@ -1,4 +1,3 @@
-
 # 1. Створити клас ОСОБА (прізвище, ім'я, по батькові, дата народження, стать та ін.).
 # 2. Визначити методи для зміни і читання значень полів даного класу.
 # 3. Перевантажити необхідні оператори.
@@ -8,6 +7,9 @@
 # 5. Розробити клас ОРГАНІЗАЦІЯ, що містить послідовнясть
 # об'єктів класу СЛУЖБОВЕЦЬ. Знайти кількість осіб, стаж роботи яких перевищує наперед задане значення.
 # 6. Для роботи із послідовністю об'єктів побудувати та використати ітератор.
+import uuid
+
+WORK_EXPERIENCE = 10
 
 from datetime import datetime
 
@@ -25,6 +27,7 @@ class Person:
         self._surname = surname
         self._date = date
         self._gender = gender
+        self._id = uuid.uuid1()
 
     @property
     def get_name(self):
@@ -43,37 +46,42 @@ class Person:
         return self._gender
 
     @get_name.setter
-    def get_name(self, name):
+    def name_setter(self, name):
         self._name = name
 
-    @get_surname
-    def get_surname(self, surname):
+    @get_surname.setter
+    def surname_setter(self, surname):
         self._surname = surname
 
     @get_date.setter
-    def get_date(self, date):
+    def date_setter(self, date):
         self._date = date
 
     @get_gender.setter
-    def get_gender(self, gender):
+    def gender_setter(self, gender):
         self._gender = gender
+
+    def __str__(self):
+        return f'Name: {self._name}\tSurname: {self._surname} -- {self._gender}\nDate of bd: {self._date}'
 
 
 class Worker(Person):
-    def __init__(self, name, surname, date, gender, oraganization, specialty, position, salary, work_experience):
+    def __init__(self, name, surname, date, gender, organization, specialty, position, salary, work_experience):
         super(Worker, self).__init__(name, surname, date, gender)
         if not (isinstance(salary, int) or isinstance(work_experience, int)):
             raise ValueError('invalid input')
         if not isinstance(position, str) or len(position) < 1:
-            raise ValueError('inva;id input')
-        self._oraganization = oraganization
+            raise ValueError('invalid position input')
+        if salary < 0 or 40 < work_experience < 0:
+            raise ValueError('invalid salary/work_experience input')
+        self._organization = organization
         self._specialty = specialty
         self._position = position
         self._salary = salary
         self._work_experience = work_experience
 
     @property
-    def get_oraganization(self):
+    def get_organization(self):
         return self._oraganization
 
     @property
@@ -92,31 +100,86 @@ class Worker(Person):
     def get_work_experience(self):
         return self._work_experience
 
-    @get_oraganization.setter
-    def get_oraganization(self, org):
-        self._oraganization = org
+    @get_organization.setter
+    def organization_setter(self, org):
+        self._organization = org
 
     @get_specialty.setter
-    def get_specialty(self, speciality):
+    def specialty_setter(self, speciality):
         self._specialty = speciality
 
     @get_position.setter
-    def get_position(self, position):
+    def position_setter(self, position):
         self._position = position
 
     @get_salary.setter
-    def get_salary(self, salary):
+    def salary_setter(self, salary):
         self._salary = salary
 
     @get_work_experience.setter
-    def get_work_experience(self, exp):
+    def work_experience_setter(self, exp):
         self._work_experience = exp
+
+    def __str__(self):
+        return f'Worker:\nName: {self._name}\tSurname: {self._surname} -- {self._gender}\nDate of bd: {self._date}\n' \
+               f'{self._organization}:\n\tPosition: {self._position}\t' \
+                                     f'Salary: {self._salary}\n\tDiploma: {self._specialty}\t' \
+                                     f'Experience:{self._work_experience}'
 
 
 class Organization:
-    def __init__(self, name, location, workers_amount):
+    def __init__(self, name, location, *args):
+        if not isinstance(name, str) or len(name) < 1:
+            raise ValueError('invalid name input')
+        if not isinstance(location, str) or len(location) < 1:
+            raise ValueError('invalid location input')
         self._name = name
         self._location = location
-        self._worker_amount = workers_amount
-        self._workers = []
+        self._workers = list(args)
+        self._counter = 0
 
+    def add_worker(self, worker):
+        if not isinstance(worker, Worker):
+            raise ValueError('invalid input')
+        self._workers.append(worker)
+
+    def __delete__(self, instance):
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._counter < len(self._workers):
+            return self._workers[self._counter]
+            self._counter += 1
+        raise StopIteration
+
+    def __str__(self):
+        return f'{self._name}\nLocation: {self._location}' + '\n'.join(str(i) for i in self._workers)
+
+
+Worker_1 = Worker('N', 'SN', '12.10.1999', 'male', 'SMTH', 'dsg', 'aaaa', 23444, 3)
+Worker_2 = Worker('K', 'Surname', '12.10.1999', 'male', 'SMTH', 'FGH', 'DFG', 23444, 3)
+Worker_3 = Worker('F', 'Surname', '12.10.1999', 'male', 'SMTH', 'FGH', 'DFG', 23444, 3)
+Worker_4 = Worker('L', 'Surname', '12.10.1999', 'male', 'SMTH', 'FGH', 'DFG', 23444, 3)
+Worker_5 = Worker('M', 'Surname', '12.10.1999', 'male', 'SMTH', 'FGH', 'DFG', 23444, 3)
+
+Organization_1 = Organization('SOME_NAME', 'Somewhere', Worker_1, Worker_2, Worker_3, Worker_4, Worker_5)
+
+# print(Organization_1)
+#
+
+for i in Organization_1:
+    print(i)
+
+
+
+# def f(*arg):
+#     return type(arg)
+# print(f(3, 4, 5, 6, 8))
+
+# a = (4, 5, 7, 7, 8)
+# a = list(a)
+# a.append(2324)
+# print(a)
